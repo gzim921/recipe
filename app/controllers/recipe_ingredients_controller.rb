a18fd25e7458b2dbe0014a44ca0fd25eaa895369
gzim921 <gzimiseni7@gmail.com>
2020-08-16 15:02:12 +0200

    Add ingredients to recipe

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -3,16 +3,23 @@ class RecipeIngredientsController < ApplicationController
 
   def new
     if @recipe = Recipe.find_by_id(params[:recipe_id])
-      @recipe_ingredient = @recipe.recipe_ingredients.build
+      @recipe_ingredient = @recipe.recipe_ingredients.new
     else
       @recipe_ingredient = RecipeIngredient.new
     end
 
-    @ingredients = Ingredient.all
+    @ingredient = Ingredient.new
   end
 
   def create
     @recipe_ingredient = RecipeIngredient.create(recipe_ingredient_params)
+
+    if recipe_ingredient_params[:ingredient_id] == ""
+      Ingredient.create(recipe_ingredient_params[:ingredient_attributes])
+    else
+      @recipe_ingredient.ingredient = Ingredient.find_by_id(recipe_ingredient_params[:ingredient_id])
+    end
+
     if @recipe_ingredient.save
       redirect_to recipes_path
     else
@@ -21,7 +28,7 @@ class RecipeIngredientsController < ApplicationController
   end
 
   private
-  
+
   def recipe_ingredient_params
     params.require(:recipe_ingredient).permit(:recipe_id, :ingredient_id, :quantity, ingredient_attributes: [:name] )
   end
