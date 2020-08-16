482a4482bb58ffbc4e5dd9ef34af340ae04fa29e
gzim921 <gzimiseni7@gmail.com>
2020-08-16 15:28:28 +0200

    Add functionality to create categories and newest scope method in recipe

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,19 @@
 class CategoriesController < ApplicationController
   before_action :require_login
+  before_action :admin_only, except: [:index, :show]
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+    if @category.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
 
   def index
     @categories = Category.all
@@ -7,5 +21,12 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find_by_id(params[:id])
+    @recipes = @category.recipes
+  end
+
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
