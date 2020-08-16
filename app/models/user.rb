f9e37ef4008493a58cf5d996f7615a277dbc24ac
gzim921 <gzimiseni7@gmail.com>
2020-08-16 13:55:45 +0200

    Add sign up, login and log out functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :comments
   has_many :ratings
+
+  validates :user_name, uniqueness: true, presence: true
+  validates :email, presence: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  resources :users
+
+  resources :categories do
+    resources :recipes
+  end
+
+  resources :recipes do
+    resources :ingredients
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
