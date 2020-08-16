56ce95f114f8dc77f05f9ef91a2ddf27a341c834
gzim921 <gzimiseni7@gmail.com>
2020-08-16 14:23:50 +0200

    Added models attribute and view refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :ratings
 
-  validates :user_name, uniqueness: true, presence: true
-  validates :email, presence: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :user_name, presence: true
+  validates :email, uniqueness: true, presence: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :email, confirmation: true
 
   has_secure_password
 end
