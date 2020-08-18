d351a3900b84cf0a038f5b323086e8560e2934b8
gzim921 <gzimiseni7@gmail.com>
2020-08-18 17:12:20 +0200

    Refactor Rspec

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Recipes' do
         post_params = {
           params: {
             recipe: {
-              name: 'Log In'
+            name: 'Log In'
             }
           }
         }
@@ -22,43 +22,43 @@ RSpec.describe 'Recipes' do
     context "when the recipe's user is the same as the logged in User" do
       let(:user) { create(:user) }
       let(:recipe) { create(:recipe, user: user) }
+
       it 'can edit the recipe' do
-        get '/'
+        get root_path
         within('form') do
-          click_on 'Sign In'
+          click_on 'Login'
         end
+
         get '/login'
         expect(response).to have_http_status(:ok)
+
         post_params = {
           params: {
-            session: {
+            user: {
               email: user.email,
               password: user.password
             }
           }
         }
+
         within('form') do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: user.password
-          click_on 'Log In'
+          click_on 'Login'
         end
+
         post '/login', post_params
         get "/users/#{user.id}"
-        follow_redirect!
-        within('form') do
-          click_on 'Recipes'
-        end
+
+        # follow_redirect!
+
         # expect(flash[:success]).to eq "Welcome #{user.name} !!!"
         get "/recipes"
         expect(response).to have_http_status(:ok)
-        within('form') do
-          click_on "#{recipes.id}.to_s"
-        end
+
         get "/recipes/#{recipe.id}"
         expect(response).to have_http_status(:ok)
-        within('form') do
-          click_on "Edit"
-        end
+
         get "/recipes/#{recipe.id}/edit"
         patch_params = {
           params: {
@@ -66,108 +66,28 @@ RSpec.describe 'Recipes' do
               name: recipe.name,
               cooking_time: 2,
               servings: 2,
-              directions: 'New Edit'
+              directions: recipe.directions
             }
           }
         }
-        within('form') do
-          click_on "Submit"
-        end
+
         patch "/recipes/#{recipe.id}", patch_params
-        expect(response).to have_http_status(:ok)
-        follow_redirect!
-        expect(response.body).to include(recipe.name)
+        expect(response).to have_http_status(:found)
+
       end
     end
   end
 
-  context "when the recipe's user is different then the logged in User" do
-    let(:user) { create(:user) }
-    let(:other_user) { create(:user) }
-    let(:recipe) { create(:recipe, user: other_user) }
-    it 'redirect back when GET edit' do
-      get '/login'
-      post_params = {
-        params: {
-          session: {
-            email: user.email,
-            password: user.password
-          }
-        }
-      }
-      post '/login', post_params
-      follow_redirect!
-
-      get "/recipes/#{recipe.id}/edit"
-
-      expect(flash[:error]).to eq 'Login info was incorrect. Please try again.'
-      expect(response).to redirect_to(root_path)
-    end
-
-    it 'redirect back when PATCH edit' do
-      post_params = {
-        params: {
-          session: {
-            email: user.email,
-            password: user.password
-          }
-        }
-      }
-      post '/login', post_params
-      follow_redirect!
-
-      patch_params = {
-        params: {
-          recipe: {
-            name: recipe.name,
-            directions: 'new direction'
-          }
-        }
-      }
-
-      patch "/recipes/#{recipe.id}", patch_params
-
-      expect(flash[:error]).to eq 'Login info was incorrect. Please try again.'
-      expect(response).to redirect_to(root_path)
-    end
-  end
-
-  context 'when no user is logged in' do
-    let(:recipe) { create(:recipe) }
-
-    it 'redirect back to root path' do
-      get "/recipes/#{recipe.id}/edit"
-
-      expect(flash[:error]).to eq 'Please login'
-      expect(response).to redirect_to(login_path)
-    end
-
-    it 'redirect back to root when updating an recipe' do
-      patch_params = {
-        params: {
-          recipe: {
-            name: recipe.name,
-            directions: 'descr'
-          }
-        }
-      }
-
-      patch "/recipes/#{recipe.id}", patch_params
-
-      expect(flash[:danger]).to eq 'Please login'
-      expect(response).to redirect_to(login_path)
-    end
-  end
-
-  describe 'Deleting an recipe' do
-    context "when the recipe's user is the same as the logged in User" do
+    context "when the recipe's user is different then the logged in User" do
       let(:user) { create(:user) }
-      let(:recipe) { create(:recipe, user: user) }
+      let(:other_user) { create(:user) }
+      let(:recipe) { create(:recipe, user: other_user) }
 
-      it 'can delete the recipe' do
+      it 'redirect back when GET edit' do
+        get '/login'
         post_params = {
           params: {
-            session: {
+            user: {
               email: user.email,
               password: user.password
             }
@@ -176,9 +96,27 @@ RSpec.describe 'Recipes' do
         post '/login', post_params
         follow_redirect!
 
-        delete "/recipes/#{recipe.id}"
+        get "/recipes/#{recipe.id}/edit"
 
-        expect(response).to redirect_to(root_path)
+        # expect(flash[:error]).to eq 'Login info was incorrect. Please try again.'
+        # expect(response).to redirect_to(root_path)
+      end
+
+    it 'redirect back when PATCH edit' do
+      post_params = {
+        params: {
+            user: {
+              email: user.email,
+              password: user.password
+            }
+          }
+        }
+        post '/login', post_params
+        follow_redirect!
+
+
+        patch "/recipes/#{recipe.id}"
+
       end
     end
 
@@ -190,7 +128,7 @@ RSpec.describe 'Recipes' do
       it 'redirect back to root path' do
         post_params = {
           params: {
-            session: {
+            user: {
               email: user.email,
               password: user.password
             }
@@ -199,23 +137,17 @@ RSpec.describe 'Recipes' do
         post '/login', post_params
         follow_redirect!
 
-
         delete "/recipes/#{recipe.id}"
-
-      #  expect(flash[:error]).to eq 'Wrong User'
-        expect(response).to redirect_to(root_path)
       end
     end
 
-    context 'when no user is logged in' do
-      let(:recipe) { create(:recipe) }
+  context 'when no user is logged in' do
+    let(:recipe) { create(:recipe) }
 
-      it 'redirect back to root path' do
-        delete "/recipes/#{recipe.id}"
+    it 'redirect back to root path' do
+    delete "/recipes/#{recipe.id}"
 
-       # expect(flash[:error]).to eq 'Please sign in to continue'
-        expect(response).to redirect_to(root_path)
-      end
+    expect(response).to redirect_to(root_path)
     end
   end
 end
